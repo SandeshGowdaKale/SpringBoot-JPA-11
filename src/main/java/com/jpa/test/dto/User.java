fd56e54e06187cd3cdc96d9f7ccd8e0a81b0c1a6
sandesh.gowda <sandeshgowda2021@gmail.com>
2022-11-29 14:18:26 +0530

    repository

--- src/main/java/com/jpa/test/dto/User.java
+++ src/main/java/com/jpa/test/dto/User.java
@@ -7,16 +7,14 @@ import javax.persistence.Id;
 
 @Entity
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
 	private String city;
 	private String status;
-	
-	
-	
+
 	public User(int id, String name, String city, String status) {
 		super();
 		this.id = id;
@@ -24,37 +22,42 @@ public class User {
 		this.city = city;
 		this.status = status;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", city=" + city + ", status=" + status + "]";
 	}
-	
-	
 
-	
-	
 }
